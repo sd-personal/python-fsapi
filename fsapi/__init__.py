c4748c988ab2538a15ecd33f3c595590739f3ea8
Stijn Dickx <82111073+sd-personal@users.noreply.github.com>
2021-04-13 00:43:37 +0200

    fsapi version 0.1.1: Fixes for presets

--- fsapi/__init__.py
+++ fsapi/__init__.py
@@ -257,19 +257,13 @@ class FSAPI(object):
 
     # GET preset call does not exist but play.info.name can be matched with the names in the presets list
     def get_preset(self):
-        preset = None
-        int_preset = self.handle_long('netRemote.play.info.name')
-        for temp_preset in self.presets:
-            if temp_preset['band'] == int_preset:
-                preset = temp_preset['label']
-
-        return str(preset)
+        return self.play_info_name()
 
     def set_preset(self, value):
         preset = -1
         for preset in self.presets:
             if temp_preset['label'] == value:
-                mode = temp_mode['band']
+                preset = temp_mode['band']
 
         self.handle_set('netRemote.nav.action.selectPreset', preset)
 
