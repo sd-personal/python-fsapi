6b45ad71e582299d2037a36051307714b48c8fdd
Stijn Dickx <82111073+sd-personal@users.noreply.github.com>
2024-12-04 19:31:02 +0100

    fsapi version 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 PACKAGES = find_packages(exclude=['tests', 'tests.*'])
 
 REQUIRES = [
-    'requests>=2,<3'
+    'requests>=2,<3',
     'lxml>=5,<6'
 ]
 
@@ -15,26 +15,21 @@ PROJECT_CLASSIFIERS = [
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.0',
-    'Programming Language :: Python :: 3.1',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Software Development :: Libraries'
 ]
 
 
 
 setup(name='fsapi',
-      version='0.2.2',
+      version='0.2.3',
       description='Implementation of the Frontier Silicon API for Python',
       author='Krasimir Zhelev',
       author_email='krasimir.zhelev@gmail.com',
       keywords='fsapi frontier silicon',
       license="Apache License 2.0",
-      download_url='https://github.com/sd-personal/python-fsapi/archive/0.2.2.zip',
+      download_url='https://github.com/sd-personal/python-fsapi/archive/0.2.3.zip',
       url='https://github.com/sd-personal/python-fsapi.git',
       maintainer='sd-personal',
       zip_safe=True,
